079a9326b214ecd6ae3bee9fb3d96b3848d311c4
Jaime Freire <5436581+jaimefreire@users.noreply.github.com>
2024-03-31 11:48:45 +0200

    Initial commit

---
+++ src/main/java/dev/freireservices/pf/Application.java
@@ -0,0 +1,13 @@
+package dev.freireservices.pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
